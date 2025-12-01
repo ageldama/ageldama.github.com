3dbe3f08d5ac90ab0e4e5f3e6a51a71cad4b0f7e
jonghyouk yun <ageldama@gmail.com>
2025-12-01 15:26:30 +0900

    --wip-- [skip ci]

---
+++ js/search.js
@@ -0,0 +1,92 @@
+
+
+
+function resetSearchResults() {
+    $('.search-results').empty();
+    $('.search-loading-indicator').hide();
+    $('main').fadeIn();
+    // $('main.list').fadeIn();
+}
+
+var showSearchResults_template = null;
+function showSearchResults(searchTerm, searchResults) {
+    $('.search-results').empty();
+    $('.search-loading-indicator').hide();
+    $('main').fadeOut();
+    // $('main.list').fadeOut();
+
+    const $resultsContainer = $('.search-results');
+
+    const templ = `
+<h2>검색어(search term): <%= searchTerm %> /
+<% if (searchResults.length === 0) { %>
+    <span>--- 검색결과 없음 ---</span>
+<% } else { %>
+    <span>--- 검색결과: <%= searchResults.length %>건 ---</span>
+<% } %>
+</h2>
+
+<% if (searchResults.length > 0) { %>
+    <ol>
+        <% searchResults.forEach(function(row, idx){ %>
+            <li><a href="<%= row.permalink %>"><em class='search-result-row'><%= row.title %></em></a>
+                <div><%= row.contents %></div></li>
+        <% }); %>
+    </ol>
+<% } %>
+`;
+
+    if (!showSearchResults_template) {
+        showSearchResults_template = _.template(templ);
+    }
+
+    const expanded = showSearchResults_template({
+        searchTerm, searchResults,
+    });
+
+    $resultsContainer.append($(expanded));
+
+    const mark = new Mark(document.querySelectorAll('.search-results'));
+    mark.mark(searchTerm);
+}
+
+
+function searchTermMatch(searchTerm, str) {
+    return _.toLower(_.toString(str)).indexOf(_.toLower(searchTerm)) > -1;
+}
+
+function executeSearch(searchTerm) {
+    $('.search-loading-indicator').show();
+
+    const ts = document.querySelector('meta[name="hugo-gen-timestamp"]').content;
+
+    fetch("/index.json?" + String(ts))
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            const filtered = _.filter(data, function(row) {
+                return row && (searchTermMatch(searchTerm, row['title'])
+                               || searchTermMatch(searchTerm, row['contents']));
+            });
+            // console.log(filtered);
+            showSearchResults(searchTerm, filtered);
+        })
+}
+
+
+
+$(function(){
+    const $input = $('#search-query');
+    $input.
+        on('keyup', _.debounce(function() {
+            const q = $input.val();
+            // console.log('query: ' + q);
+            if (_.isEmpty(q)) {
+                resetSearchResults(q);
+            } else {
+                executeSearch(q);
+            }
+        }, 500));
+});
+
